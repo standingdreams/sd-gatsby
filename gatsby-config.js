ac5aa6c3b49d89933b14765b1652c20a7c14b21c
Douglas Rogers <designer.drogers@gmail.com>
2021-07-22 22:50:04 -0400

    Merge conflict fix
    Conflict with package-lock.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,5 +81,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `${process.env.GOOGLE_TRACKING_ID}`
+        ],
+      },
+    },
   ],
 }
