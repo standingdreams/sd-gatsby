88f40651e57048e09b5f786e9abbbd3923971335
Douglas Rogers <designer.drogers@gmail.com>
2021-07-22 20:44:15 -0400

    ESLint changes

--- src/pages/Colophon.js
+++ src/pages/Colophon.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
-import { useStaticQuery } from 'gatsby';
-import ContactBlock from '../components/contactBlock';
+import ContactBlock from "../components/contactBlock"
 
 export default function Colophon() {
-  const {allPrismicColophon} = useStaticQuery(graphql`
+  const { allPrismicColophon } = useStaticQuery(graphql`
     {
       allPrismicColophon {
         nodes {
@@ -19,7 +19,7 @@ export default function Colophon() {
       }
     }
   `)
-  const {text_above_title, page_title, content} = allPrismicColophon.nodes[0].data
+  const { text_above_title, page_title, content } = allPrismicColophon.nodes[0].data
   return (
     <Layout title="Colophon">
       <div className="profile-wrapper">
@@ -28,7 +28,7 @@ export default function Colophon() {
             <div className="profile__aboutWrap">
               <h1 className="el-h2">{text_above_title}</h1>
               <h2 className="el-h1">{page_title}</h2>
-              <div dangerouslySetInnerHTML={{__html: content.html}}></div>
+              <div dangerouslySetInnerHTML={{ __html: content.html }} />
             </div>
           </div>
         </div>
